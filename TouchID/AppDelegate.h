83d385c71b6762a5e42e35aeac7bb33e6f61a0bf
jianshuaibo <jianshuaibo@microseer.com>
2017-02-22 10:18:09 +0800

    Initial Commit

---
+++ TouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchID
+//
+//  Created by 微知 on 2017/2/22.
+//  Copyright © 2017年 Jame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchID
+//
+//  Created by 微知 on 2017/2/22.
+//  Copyright © 2017年 Jame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
